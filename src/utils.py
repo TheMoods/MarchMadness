4f42c1e4eadb1144cef6c3873b345f0fa6dd721d
Westermann <westermann93@googlemail.com>
2018-02-25 20:27:08 +0100

    :bug: Fixed data loader and load all data with targets

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_target_sample():
@@ -8,21 +8,32 @@ def load_target_sample():
     target['Season'] = target.index.map(lambda i: i[:4])
     target['team_a'] = target.index.map(lambda i: i[5:9])
     target['team_b'] = target.index.map(lambda i: i[10:14])
-    target['DayNum'] = np.nan
     target['in_target'] = True
+    target['game_set'] = 'ncaa'
     return target
 
 
-def load_data_template():
-    data = pd.read_csv('data/NCAATourneyCompactResults.csv')
-    target = load_target_sample()
+def load_data_template(season=False):
+    data = [pd.read_csv('data/NCAATourneyCompactResults.csv')]
+    data = pd.concat(data).astype({
+        'Season': str, 'WTeamID': str, 'LTeamID': str
+    })
+    data['game_set'] = 'ncaa'
     data['team_a'] = data[['WTeamID', 'LTeamID']]\
         .apply(lambda t: t[0] if int(t[0]) < int(t[1]) else t[1], axis=1)
     data['team_b'] = data[['WTeamID', 'LTeamID']]\
         .apply(lambda t: t[0] if int(t[0]) > int(t[1]) else t[1], axis=1)
     data['a_win'] = data['WTeamID'] == data['team_a']
-    data = data[['Season', 'team_a', 'team_b', 'a_win', 'DayNum']]
-    data['in_target'] = False
-    data = pd.concat([data, target.reset_index(drop=True)])
-    data = data.astype({'Season': str, 'team_a': str, 'team_b': str})
+    data = data[['Season', 'team_a', 'team_b', 'a_win', 'DayNum', 'game_set']]
+    target = load_target_sample()
+    target_index = target.index.tolist()
+    data['in_target'] = data[['Season', 'team_a', 'team_b']]\
+        .apply(lambda r: '_'.join(r.values) in target_index, axis=1)
+    data = pd.merge(target, data,
+                    on=['Season', 'team_a', 'team_b', 'game_set', 'in_target'], how='outer')
+    data = data.astype({
+        'Season': str, 'team_a': str, 'team_b': str,
+        'in_target': bool
+    })
+    data['game_set'] = LabelEncoder().fit_transform(data['game_set'])
     return data
