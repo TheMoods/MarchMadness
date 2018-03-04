72a253860025ccc9827d36c451bdf7907878e033
Javier Mas Adell <javier.mas@barcelonagse.eu>
2018-03-04 17:31:14 +0100

    :bug: checked out code in utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,9 @@ from sklearn.preprocessing import LabelEncoder
 def load_target_sample():
     target = pd.read_csv('data/SampleSubmissionStage1.csv').set_index('ID')\
         .drop('Pred', axis=1)
-    target['Season'] = target.index.map(lambda i: i.split('_')[0])
-    target['team_a'] = target.index.map(lambda i: i.split('_')[1])
-    target['team_b'] = target.index.map(lambda i: i.split('_')[2])
+    target['Season'] = target.index.map(lambda i: i[:4])
+    target['team_a'] = target.index.map(lambda i: i[5:9])
+    target['team_b'] = target.index.map(lambda i: i[10:14])
     target['in_target'] = True
     target['game_set'] = 'ncaa'
     return target
@@ -21,8 +21,16 @@ def load_data_template(season=False):
         season_games = pd.read_csv('data/RegularSeasonCompactResults.csv')
         season_games['game_set'] = 'season'
         data.append(season_games)
-    data = pd.concat(data)
-    data = generate_data_template(extra_cols='game_set')
+
+    data = pd.concat(data).astype({
+        'Season': str, 'WTeamID': str, 'LTeamID': str
+    })
+    data['team_a'] = data[['WTeamID', 'LTeamID']]\
+        .apply(lambda t: t[0] if int(t[0]) < int(t[1]) else t[1], axis=1)
+    data['team_b'] = data[['WTeamID', 'LTeamID']]\
+        .apply(lambda t: t[0] if int(t[0]) > int(t[1]) else t[1], axis=1)
+    data['a_win'] = data['WTeamID'] == data['team_a']
+    data = data[['Season', 'team_a', 'team_b', 'a_win', 'DayNum', 'game_set']]
     target = load_target_sample()
     target_index = target.index.tolist()
     data['in_target'] = data[['Season', 'team_a', 'team_b']]\
@@ -34,17 +42,4 @@ def load_data_template(season=False):
         'in_target': bool
     })
     data['game_set'] = LabelEncoder().fit_transform(data['game_set'])
-    return data
-
-
-def generate_data_template(data, extra_cols):
-    data = data.astype({
-        'Season': str, 'WTeamID': str, 'LTeamID': str
-    })
-    data['team_a'] = data[['WTeamID', 'LTeamID']]\
-        .apply(lambda t: t[0] if int(t[0]) < int(t[1]) else t[1], axis=1)
-    data['team_b'] = data[['WTeamID', 'LTeamID']]\
-        .apply(lambda t: t[0] if int(t[0]) > int(t[1]) else t[1], axis=1)
-    data['a_win'] = data['WTeamID'] == data['team_a']
-    data = data[['Season', 'team_a', 'team_b', 'a_win', 'DayNum'] + extra_cols]
     return data
