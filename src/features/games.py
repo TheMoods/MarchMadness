f7010163d18d03a35ebac8ac408c00fdb603673f
Westermann <westermann93@googlemail.com>
2018-03-07 18:15:32 +0100

    Generalize game features

--- src/features/games.py
+++ src/features/games.py
@@ -10,6 +10,10 @@ class GameFeatures(Feature):
                 .load_game_data('NCAATourneyCompactResults.csv')
         self.season_games = self\
                 .load_game_data('RegularSeasonCompactResults.csv')
+        self.season_games = self\
+                .load_game_data('RegularSeasonCompactResults.csv')
+        self.conference_games = self\
+                .load_game_data('ConferenceTourneyGames.csv')
         self.average_rankings = self\
             .load_ranks('MasseyOrdinals.csv')
         
@@ -18,9 +22,15 @@ class GameFeatures(Feature):
         games = games.astype({
             'LTeamID': str,
             'WTeamID': str,
-            'Season': str
+            'Season': str,
+            'DayNum': str
             })
-        games['diff'] = games['WScore'] - games['LScore']
+        if any(c in games.columns for c in ['WScore', 'LScore']):
+            games['diff'] = games['WScore'] - games['LScore']
+        else:
+            games['diff'] = 0
+
+        games.DayNum.fillna(366, inplace=True)
         return games
     
     def load_ranks(self, path):
@@ -39,6 +49,15 @@ class GameFeatures(Feature):
         games_won_in_season = self.lag_features(games_won_in_season,
                 drop_unlagged=False)
         return games_won_in_season
+
+    def games_won_against_opponent(self, df, team,
+            name='games_won_against_opponent'):
+        games_won_against_opponent = self.season_games\
+                .groupby(['WTeamID', 'LTeamID', 'Season']).count()[['diff']]\
+                .rename(columns={'diff': '{}_{}'.format(name, team)})
+        games_won_against_opponent = self.lag_features(games_won_against_opponent,
+                drop_unlagged=False)
+        return games_won_against_opponent
 
     def games_won_in_season_against_opponent(self, df, team,
             name='games_won_in_season_against_opponent'):
@@ -66,6 +85,24 @@ class GameFeatures(Feature):
         games_won_in_tourney_against_opponent = self.lag_features(games_won_in_tourney_against_opponent,
                 drop_unlagged=True)
         return games_won_in_tourney_against_opponent
+
+    def games_won_in_conference(self, df, team,
+            name='games_won_in_conference'):
+        games_won_in_conference = self.conference_games\
+                .groupby(['WTeamID', 'Season']).count()[['diff']]\
+                .rename(columns={'diff': '{}_{}'.format(name, team)})
+        games_won_in_conference = self.lag_features(games_won_in_conference,
+                drop_unlagged=False)
+        return games_won_in_conference
+
+    def games_won_in_conference_against_opponent(self, df, team,
+            name='games_won_in_conference_against_opponent'):
+        games_won_in_conference_against_opponent = self.conference_games\
+                .groupby(['WTeamID', 'LTeamID', 'Season']).count()[['diff']]\
+                .rename(columns={'diff': '{}_{}'.format(name, team)})
+        games_won_in_conference_against_opponent = self.lag_features(games_won_in_conference_against_opponent,
+                drop_unlagged=False)
+        return games_won_in_conference_against_opponent
 
     def average_ranking_team(self, df, team, 
              name='average_ranking'):
