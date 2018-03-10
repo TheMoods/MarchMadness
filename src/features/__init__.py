d7e2fd16d027bfdd6599dd7b050d0cc77ef8c88e
Westermann <westermann93@googlemail.com>
2018-03-10 20:37:18 +0100

    Make Features Classes directly loadable

---
+++ src/features/__init__.py
@@ -0,0 +1,7 @@
+from src.features.ensemble import *
+from src.features.events import *
+from src.features.feature import *
+from src.features.games import *
+from src.features.games_detailed import *
+from src.features.rankings import *
+from src.features.seeds import *
